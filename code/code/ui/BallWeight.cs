5c33f97facc49c6bf771caa8fa7efccb0af1ef49
kote2345 <62496056+kote2345@users.noreply.github.com>
2021-05-08 12:07:24 +0400

    Add files via upload

---
+++ code/code/ui/BallWeight.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Threading.Tasks;
+
+namespace BallsWars
+{
+	public partial class BallWeight : Panel
+	{
+		private Label Weight;
+
+		public BallWeight()
+		{
+			Add.Label( "Weight: " );
+			Weight = Add.Label( "5", "value" );
+		}
+
+		public override void Tick()
+		{
+			if ( Sandbox.Player.Local is not Player player ) return;
+			SetClass( "spectator", (player as BallPlayer).Spectator == true );
+		}
+	}
+}
